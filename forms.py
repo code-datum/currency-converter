622b0ee2df29c8df11b2953217b3d66c15fe456f
Maxat Uralov <code.datum@gmail.com>
2020-02-27 20:03:54 +0600

    Update form and inplement WTForms field and validate

--- forms.py
+++ forms.py
@@ -1,33 +1,9 @@
-from flask_wtf import Form
-from wtforms import TextField, PasswordField
-from wtforms.validators import DataRequired, EqualTo, Length
+from flask_wtf import FlaskForm
+from wtforms import DecimalField, SubmitField, SelectField
+from wtforms.validators import DataRequired
 
-# Set your classes here.
-
-
-class RegisterForm(Form):
-    name = TextField(
-        'Username', validators=[DataRequired(), Length(min=6, max=25)]
-    )
-    email = TextField(
-        'Email', validators=[DataRequired(), Length(min=6, max=40)]
-    )
-    password = PasswordField(
-        'Password', validators=[DataRequired(), Length(min=6, max=40)]
-    )
-    confirm = PasswordField(
-        'Repeat Password',
-        [DataRequired(),
-        EqualTo('password', message='Passwords must match')]
-    )
-
-
-class LoginForm(Form):
-    name = TextField('Username', [DataRequired()])
-    password = PasswordField('Password', [DataRequired()])
-
-
-class ForgotForm(Form):
-    email = TextField(
-        'Email', validators=[DataRequired(), Length(min=6, max=40)]
-    )
+class ExchangeForm(FlaskForm):
+    currency_val = DecimalField('Currency value', validators=[DataRequired()])
+    select_currency = SelectField(u'Select currency', choices=[('CAD', 'CAD'), ('USD', 'USD')])
+    select_converted = SelectField(u'Select currency', choices=[('CAD', 'CAD'), ('USD', 'USD')])
+    submit = SubmitField('Convert')
