5205f5e35ec2586cb630238cd5310358b5cf4e2b
Maxat <code.datum@gmail.com>
2020-02-28 17:17:34 +0600

    Add currency code

--- forms.py
+++ forms.py
@@ -2,8 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import FloatField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
+
 class ExchangeForm(FlaskForm):
+    CURRENCY_CODE = [('CZK', 'CZK'), ('USD', 'USD'), ('PLN', 'PLN'), ('EUR', 'EUR')]
     currency_val = FloatField('Currency value', validators=[DataRequired()])
-    select_currency = SelectField(u'Select currency', choices=[('CAD', 'CAD'), ('USD', 'USD')])
-    select_converted = SelectField(u'Select currency', choices=[('CAD', 'CAD'), ('USD', 'USD')])
+    select_currency = SelectField(u'Select currency', choices=CURRENCY_CODE)
+    select_converted = SelectField(u'Select currency', choices=CURRENCY_CODE)
     submit = SubmitField('Convert')
